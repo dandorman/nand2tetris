c25fb9db1f332abc8a1498124b4ab425e0119f1f
Dan Dorman <dan.dorman@gmail.com>
2014-04-21 21:44:41 -0600

    Finish Project 4: Mult

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -6,4 +6,45 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)
 
-// Put your code here.
+// initialize i with value in R1
+@1
+D=M
+@i
+M=D
+
+// initialize product with 0
+@0
+D=A
+@product
+M=D
+
+(LOOP)
+  // if i <= 0, break out of LOOP
+  @i
+  D=M
+  @END
+  D;JLE
+
+  // decrement i
+  D=D-1
+  @i
+  M=D
+
+  // load and add R0 to product
+  @product
+  D=M
+  @0
+  D=D+M
+  @product
+  M=D
+
+  // restart loop
+  @LOOP
+  0;JMP
+(END)
+
+// load product into R2
+@product
+D=M
+@2
+M=D
